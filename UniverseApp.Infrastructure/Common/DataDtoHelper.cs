582202b7f747451c3a5f04f0bf6c1b74f72b608b
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-10-24 19:58:16 +0300

    DataDtoHelper Class With GetEntityDtoInfoAsync Method Implemented

---
+++ UniverseApp.Infrastructure/Common/DataDtoHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using UniverseApp.Infrastructure.Data.DTOs;
+
+namespace UniverseApp.Infrastructure.Common
+{
+    internal class DataDtoHelper
+    {
+        internal static async Task<List<T>> GetEntityDtoInfoAsync<T>()
+        {
+            List<T> entityDtoList = new List<T>();
+
+            var result = new ResultTDto<T>()
+            { Next = "" };
+
+            var client = new HttpClient();
+
+            string url = (typeof(T).Name) switch
+            {
+                "PlanetInfoDto" => url = "https://swapi.dev/api/planets",
+                "StarshipInfoDto" => url = "https://swapi.dev/api/starships",
+                "VehicleInfoDto" => url = "https://swapi.dev/api/vehicles",
+                "SpecieInfoDto" => url = "https://swapi.dev/api/species",
+                "MovieInfoDto" => url = "https://swapi.dev/api/films",
+                "CharacterInfoDto" => url = "https://swapi.dev/api/people"
+            };
+
+            var option = new JsonSerializerOptions
+            { PropertyNameCaseInsensitive = true };
+
+            while (result != null && result.Next != null)
+            {
+                var responce = await client.GetAsync(url);
+                var responceContent = await responce.Content.ReadAsStringAsync();
+                result = JsonSerializer.Deserialize<ResultTDto<T>>(responceContent, option);
+                entityDtoList.AddRange(result!.Results);
+                url = result.Next;
+            }
+
+            return entityDtoList;
+        }
+    }
+}
