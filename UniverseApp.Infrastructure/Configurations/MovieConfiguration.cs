f82007735a816d7898aab60c2258c95ad970297d
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-10-24 19:47:58 +0300

    ModelBuilder Extension Method Seed Created

---
+++ UniverseApp.Infrastructure/Configurations/MovieConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UniverseApp.Infrastructure.Data.Models;
+
+namespace UniverseApp.Infrastructure.Configurations
+{
+    internal class MovieConfiguration : IEntityTypeConfiguration<Movie>
+    {
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UniverseApp.Infrastructure/Extension/ModelBuilderExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UniverseApp.Infrastructure.Extension
+{
+    internal static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new MovieConfiguration());
+        }
+    }
+}
