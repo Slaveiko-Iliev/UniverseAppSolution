c10402652d6224fcbabd7c2a5c4c41eb72cb49b7
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-10-24 16:18:20 +0300

    Remove Broken Migrations, Create New

--- UniverseApp.Infrastructure/Migrations/UniverseDbContextModelSnapshot.cs
+++ UniverseApp.Infrastructure/Migrations/UniverseDbContextModelSnapshot.cs
@@ -3,20 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UniverseApp.Infrastructure.Data;
 
 #nullable disable
 
-namespace UniverseApp.Infrastructure.Data.Migrations
+namespace UniverseApp.Infrastructure.Migrations
 {
     [DbContext(typeof(UniverseDbContext))]
-    [Migration("20241024080930_PlanetEntityAdded")]
-    partial class PlanetEntityAdded
+    partial class UniverseDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -24,6 +21,21 @@ namespace UniverseApp.Infrastructure.Data.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+
+            modelBuilder.Entity("CharacterMovie", b =>
+                {
+                    b.Property<int>("CharactersId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("MoviesId")
+                        .HasColumnType("int");
+
+                    b.HasKey("CharactersId", "MoviesId");
+
+                    b.HasIndex("MoviesId");
+
+                    b.ToTable("CharacterMovie");
+                });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
                 {
@@ -227,42 +239,126 @@ namespace UniverseApp.Infrastructure.Data.Migrations
                     b.ToTable("AspNetUserTokens", (string)null);
                 });
 
+            modelBuilder.Entity("MoviePlanet", b =>
+                {
+                    b.Property<int>("MoviesId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("PlanetsId")
+                        .HasColumnType("int");
+
+                    b.HasKey("MoviesId", "PlanetsId");
+
+                    b.HasIndex("PlanetsId");
+
+                    b.ToTable("MoviePlanet");
+                });
+
+            modelBuilder.Entity("MovieSpecie", b =>
+                {
+                    b.Property<int>("MoviesId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("SpeciesId")
+                        .HasColumnType("int");
+
+                    b.HasKey("MoviesId", "SpeciesId");
+
+                    b.HasIndex("SpeciesId");
+
+                    b.ToTable("MovieSpecie");
+                });
+
+            modelBuilder.Entity("MovieStarship", b =>
+                {
+                    b.Property<int>("MoviesId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("StarshipsId")
+                        .HasColumnType("int");
+
+                    b.HasKey("MoviesId", "StarshipsId");
+
+                    b.HasIndex("StarshipsId");
+
+                    b.ToTable("MovieStarship");
+                });
+
+            modelBuilder.Entity("MovieVehicle", b =>
+                {
+                    b.Property<int>("MoviesId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("VehiclesId")
+                        .HasColumnType("int");
+
+                    b.HasKey("MoviesId", "VehiclesId");
+
+                    b.HasIndex("VehiclesId");
+
+                    b.ToTable("MovieVehicle");
+                });
+
             modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Character", b =>
                 {
                     b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+                        .HasColumnType("int")
+                        .HasComment("Character Identifier");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    b.Property<string>("BirthYear")
+                        .HasMaxLength(10)
+                        .HasColumnType("nvarchar(10)")
+                        .HasComment("Character Birth Year");
+
+                    b.Property<string>("EyeColor")
+                        .HasMaxLength(20)
+                        .HasColumnType("nvarchar(20)")
+                        .HasComment("Character Eye Color");
+
+                    b.Property<string>("Gender")
+                        .HasMaxLength(6)
+                        .HasColumnType("nvarchar(6)")
+                        .HasComment("Character Gender");
+
+                    b.Property<string>("HairColor")
+                        .HasMaxLength(20)
+                        .HasColumnType("nvarchar(20)")
+                        .HasComment("Character Hair Color");
+
+                    b.Property<int?>("Height")
+                        .HasColumnType("int")
+                        .HasComment("Character Height");
+
+                    b.Property<int?>("Mass")
+                        .HasColumnType("int")
+                        .HasComment("Character Mass");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(30)
+                        .HasColumnType("nvarchar(30)")
+                        .HasComment("Character Name");
 
                     b.Property<int?>("PlanetId")
-                        .HasColumnType("int");
+                        .HasColumnType("int")
+                        .HasComment("Character Home Planet");
+
+                    b.Property<string>("SkinColor")
+                        .HasMaxLength(20)
+                        .HasColumnType("nvarchar(20)")
+                        .HasComment("Character Skin Color");
+
+                    b.Property<string>("Url")
+                        .IsRequired()
+                        .HasMaxLength(70)
+                        .HasColumnType("nvarchar(70)")
+                        .HasComment("Character Url");
 
                     b.HasKey("Id");
 
                     b.HasIndex("PlanetId");
 
                     b.ToTable("Characters");
-                });
-
-            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.CharacterMovie", b =>
-                {
-                    b.Property<int>("CharacterId")
-                        .HasColumnType("int")
-                        .HasComment("Character Identifier");
-
-                    b.Property<int>("MovieId")
-                        .HasColumnType("int")
-                        .HasComment("Movie Identifier");
-
-                    b.HasKey("CharacterId", "MovieId");
-
-                    b.HasIndex("MovieId");
-
-                    b.ToTable("CharactersMovies", t =>
-                        {
-                            t.HasComment("CharactersMovies Entity");
-                        });
                 });
 
             modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Movie", b =>
@@ -287,9 +383,6 @@ namespace UniverseApp.Infrastructure.Data.Migrations
                         .HasColumnType("int")
                         .HasComment("Movie Episode Identifier");
 
-                    b.Property<int?>("PlanetId")
-                        .HasColumnType("int");
-
                     b.Property<string>("Producer")
                         .IsRequired()
                         .HasMaxLength(100)
@@ -306,9 +399,12 @@ namespace UniverseApp.Infrastructure.Data.Migrations
                         .HasColumnType("nvarchar(60)")
                         .HasComment("Movie Title");
 
-                    b.HasKey("Id");
+                    b.Property<string>("Url")
+                        .IsRequired()
+                        .HasMaxLength(70)
+                        .HasColumnType("nvarchar(70)");
 
-                    b.HasIndex("PlanetId");
+                    b.HasKey("Id");
 
                     b.ToTable("Movies", t =>
                         {
@@ -324,45 +420,117 @@ namespace UniverseApp.Infrastructure.Data.Migrations
 
                     b.Property<string>("Climate")
                         .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                        .HasColumnType("nvarchar(50)")
+                        .HasComment("Climate of Planet");
 
                     b.Property<string>("Gravity")
                         .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                        .HasColumnType("nvarchar(50)")
+                        .HasComment("Gravity of Planet");
 
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                        .HasColumnType("nvarchar(50)")
+                        .HasComment("Planet Name");
 
                     b.Property<int?>("OrbitalPeriod")
                         .HasMaxLength(50)
-                        .HasColumnType("int");
+                        .HasColumnType("int")
+                        .HasComment("Orbital Period of Planet");
 
                     b.Property<int?>("Population")
                         .HasMaxLength(50)
-                        .HasColumnType("int");
+                        .HasColumnType("int")
+                        .HasComment("Population of Planet");
 
                     b.Property<int?>("RotationPeriod")
                         .HasMaxLength(50)
-                        .HasColumnType("int");
+                        .HasColumnType("int")
+                        .HasComment("Rotation Period of Planet");
 
                     b.Property<decimal?>("SurfaceWater")
                         .HasMaxLength(50)
-                        .HasColumnType("decimal(5, 2)");
+                        .HasColumnType("decimal(5, 2)")
+                        .HasComment("Surface Water of Planet");
 
                     b.Property<string>("Terrain")
                         .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                        .HasColumnType("nvarchar(50)")
+                        .HasComment("Terrain of Planet");
 
                     b.Property<string>("Url")
                         .IsRequired()
                         .HasMaxLength(70)
-                        .HasColumnType("nvarchar(70)");
+                        .HasColumnType("nvarchar(70)")
+                        .HasComment("Planet Url");
 
                     b.HasKey("Id");
 
                     b.ToTable("Planets");
+                });
+
+            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Specie", b =>
+                {
+                    b.Property<int>("Id")
+                        .HasColumnType("int")
+                        .HasComment("Specie Identifier");
+
+                    b.Property<int?>("CharacterId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("CharacterId");
+
+                    b.ToTable("Species");
+                });
+
+            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Starship", b =>
+                {
+                    b.Property<int>("Id")
+                        .HasColumnType("int")
+                        .HasComment("Starship Identifier");
+
+                    b.Property<int?>("CharacterId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("CharacterId");
+
+                    b.ToTable("Starships");
+                });
+
+            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Vehicle", b =>
+                {
+                    b.Property<int>("Id")
+                        .HasColumnType("int")
+                        .HasComment("Vehicle Identifier");
+
+                    b.Property<int?>("CharacterId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("CharacterId");
+
+                    b.ToTable("Vehicles");
+                });
+
+            modelBuilder.Entity("CharacterMovie", b =>
+                {
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Character", null)
+                        .WithMany()
+                        .HasForeignKey("CharactersId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Movie", null)
+                        .WithMany()
+                        .HasForeignKey("MoviesId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
@@ -416,54 +584,108 @@ namespace UniverseApp.Infrastructure.Data.Migrations
                         .IsRequired();
                 });
 
+            modelBuilder.Entity("MoviePlanet", b =>
+                {
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Movie", null)
+                        .WithMany()
+                        .HasForeignKey("MoviesId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Planet", null)
+                        .WithMany()
+                        .HasForeignKey("PlanetsId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
+            modelBuilder.Entity("MovieSpecie", b =>
+                {
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Movie", null)
+                        .WithMany()
+                        .HasForeignKey("MoviesId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Specie", null)
+                        .WithMany()
+                        .HasForeignKey("SpeciesId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
+            modelBuilder.Entity("MovieStarship", b =>
+                {
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Movie", null)
+                        .WithMany()
+                        .HasForeignKey("MoviesId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Starship", null)
+                        .WithMany()
+                        .HasForeignKey("StarshipsId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
+            modelBuilder.Entity("MovieVehicle", b =>
+                {
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Movie", null)
+                        .WithMany()
+                        .HasForeignKey("MoviesId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Vehicle", null)
+                        .WithMany()
+                        .HasForeignKey("VehiclesId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
             modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Character", b =>
                 {
-                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Planet", null)
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Planet", "Planet")
                         .WithMany("Character")
                         .HasForeignKey("PlanetId");
+
+                    b.Navigation("Planet");
                 });
 
-            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.CharacterMovie", b =>
+            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Specie", b =>
                 {
-                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Character", "Character")
-                        .WithMany("CharactersMovies")
-                        .HasForeignKey("CharacterId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Movie", "Movie")
-                        .WithMany("CharactersMovies")
-                        .HasForeignKey("MovieId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("Character");
-
-                    b.Navigation("Movie");
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Character", null)
+                        .WithMany("Species")
+                        .HasForeignKey("CharacterId");
                 });
 
-            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Movie", b =>
+            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Starship", b =>
                 {
-                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Planet", null)
-                        .WithMany("Movies")
-                        .HasForeignKey("PlanetId");
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Character", null)
+                        .WithMany("Starships")
+                        .HasForeignKey("CharacterId");
+                });
+
+            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Vehicle", b =>
+                {
+                    b.HasOne("UniverseApp.Infrastructure.Data.Models.Character", null)
+                        .WithMany("Vehicles")
+                        .HasForeignKey("CharacterId");
                 });
 
             modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Character", b =>
                 {
-                    b.Navigation("CharactersMovies");
-                });
+                    b.Navigation("Species");
 
-            modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Movie", b =>
-                {
-                    b.Navigation("CharactersMovies");
+                    b.Navigation("Starships");
+
+                    b.Navigation("Vehicles");
                 });
 
             modelBuilder.Entity("UniverseApp.Infrastructure.Data.Models.Planet", b =>
                 {
                     b.Navigation("Character");
-
-                    b.Navigation("Movies");
                 });
 #pragma warning restore 612, 618
         }
