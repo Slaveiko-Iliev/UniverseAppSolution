3479cb4960ccbef89a4db51e3d79b723d35e0eca
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-11-13 11:24:14 +0200

    ClaimsPrincipal extension Id implemented

---
+++ UniverseApp.Infrastructure/Extension/ClaimsPrincipalExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Security.Claims
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string Id(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
