0054b0ec5192b55adbb60454c1a6ec324aa8dd98
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-11-25 22:42:33 +0200

    Movie Details view implemented.

--- UniverseApp.Core/Models/Movie/MovieDetailsViewModel.cs
+++ UniverseApp.Core/Models/Movie/MovieDetailsViewModel.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UniverseApp.Core.Models.Movie
+﻿namespace UniverseApp.Core.Models.Movie
 {
-    public class MovieDetailsViewModel : MovieFormModel
-    {
-        public int Id { get; set; }
+	public class MovieDetailsViewModel : MovieFormModel
+	{
+		public int Id { get; set; }
 
-        public ICollection<string> CharactersNames { get; set; } = new HashSet<string>();
+		public ICollection<string> CharactersNames { get; set; } = new List<string>();
 
-        public ICollection<string> PlanetsNames { get; set; } = new HashSet<string>();
+		public ICollection<string> PlanetsNames { get; set; } = new List<string>();
 
-        public ICollection<string> StarshipsNames { get; set; } = new HashSet<string>();
+		public ICollection<string> StarshipsNames { get; set; } = new List<string>();
 
-        public ICollection<string> VehiclesNames { get; set; } = new HashSet<string>();
+		public ICollection<string> VehiclesNames { get; set; } = new List<string>();
 
-        public ICollection<string> SpeciesNames { get; set; } = new HashSet<string>();
-    }
+		public ICollection<string> SpeciesNames { get; set; } = new List<string>();
+	}
 }
