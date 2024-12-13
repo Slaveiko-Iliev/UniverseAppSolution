ad3a0f41f727db27a7f1f0d82c554d6be10c9bdc
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-12-13 14:19:14 +0200

    _CharacterPartial functionality improved.

--- UniverseApp.Core/Models/Character/CharacterAllViewModel.cs
+++ UniverseApp.Core/Models/Character/CharacterAllViewModel.cs
@@ -1,9 +1,15 @@
-﻿namespace UniverseApp.Core.Models.Character
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniverseApp.Core.Models.Character
 
 {
-    public class CharacterAllViewModel : CharacterFormModel
-    {
-        public int Id { get; set; }
-        public string? ImageUrl { get; set; }
-    }
+	public class CharacterAllViewModel : CharacterFormModel
+	{
+		public int Id { get; set; }
+		public string? ImageUrl { get; set; }
+
+		public int? PlanetId { get; set; }
+		[ForeignKey(nameof(PlanetId))]
+		public UniverseApp.Infrastructure.Data.Models.Planet? Planet { get; set; }
+	}
 }
--- UniverseApp.Core/Services/CharacterService.cs
+++ UniverseApp.Core/Services/CharacterService.cs
@@ -6,73 +6,75 @@ using UniverseApp.Infrastructure.Data.Models;
 
 namespace UniverseApp.Core.Services
 {
-    public class CharacterService : ICharacterService
-    {
-        private readonly IRepository _repository;
+	public class CharacterService : ICharacterService
+	{
+		private readonly IRepository _repository;
 
-        public CharacterService(IRepository repository)
-        {
-            _repository = repository;
-        }
+		public CharacterService(IRepository repository)
+		{
+			_repository = repository;
+		}
 
-        public async Task<CharacterQueryServiceModel> GetAllCharactersAsync(string? searchMovie, string? searchSpecie, string? searchVehicle, string? searchStarship, int currentPage, int charactersPerPage)
-        {
-            var characters = _repository
-                .AllReadOnly<Character>();
+		public async Task<CharacterQueryServiceModel> GetAllCharactersAsync(string? searchMovie, string? searchSpecie, string? searchVehicle, string? searchStarship, int currentPage, int charactersPerPage)
+		{
+			var characters = _repository
+				.AllReadOnly<Character>();
 
-            if (!string.IsNullOrEmpty(searchMovie))
-            {
-                characters = characters
-                    .Where(ch => ch.Movies.Any(m => m.Title == searchMovie));
-            }
+			if (!string.IsNullOrEmpty(searchMovie))
+			{
+				characters = characters
+					.Where(ch => ch.Movies.Any(m => m.Title == searchMovie));
+			}
 
-            if (!string.IsNullOrEmpty(searchSpecie))
-            {
-                characters = characters
-                    .Where(ch => ch.Species.Any(sp => sp.Name == searchSpecie));
-            }
+			if (!string.IsNullOrEmpty(searchSpecie))
+			{
+				characters = characters
+					.Where(ch => ch.Species.Any(sp => sp.Name == searchSpecie));
+			}
 
-            if (!string.IsNullOrEmpty(searchVehicle))
-            {
-                characters = characters
-                    .Where(ch => ch.Vehicles.Any(v => v.Name == searchVehicle));
-            }
+			if (!string.IsNullOrEmpty(searchVehicle))
+			{
+				characters = characters
+					.Where(ch => ch.Vehicles.Any(v => v.Name == searchVehicle));
+			}
 
-            if (!string.IsNullOrEmpty(searchStarship))
-            {
-                characters = characters
-                    .Where(ch => ch.Starships.Any(st => st.Name == searchStarship));
-            }
+			if (!string.IsNullOrEmpty(searchStarship))
+			{
+				characters = characters
+					.Where(ch => ch.Starships.Any(st => st.Name == searchStarship));
+			}
 
-            int totalCharactersCount = await characters.CountAsync();
+			int totalCharactersCount = await characters.CountAsync();
 
-            characters = characters
-                .OrderBy(m => m.Id)
-                .Skip((currentPage - 1) * charactersPerPage)
-                .Take(charactersPerPage);
+			characters = characters
+				.OrderBy(m => m.Id)
+				.Skip((currentPage - 1) * charactersPerPage)
+				.Take(charactersPerPage);
 
-            var charactersViewModels = await characters
-                .Select(ch => new CharacterAllViewModel
-                {
-                    Id = ch.Id,
-                    Name = ch.Name,
-                    Height = ch.Height.ToString(),
-                    Mass = ch.Mass.ToString(),
-                    HairColor = ch.HairColor,
-                    SkinColor = ch.SkinColor,
-                    EyeColor = ch.EyeColor,
-                    BirthYear = ch.BirthYear,
-                    Gender = ch.Gender
-                })
-                .ToListAsync();
+			var charactersViewModels = await characters
+				.Select(ch => new CharacterAllViewModel
+				{
+					Id = ch.Id,
+					Name = ch.Name,
+					Height = ch.Height.ToString(),
+					Mass = ch.Mass.ToString(),
+					HairColor = ch.HairColor,
+					SkinColor = ch.SkinColor,
+					EyeColor = ch.EyeColor,
+					BirthYear = ch.BirthYear,
+					Gender = ch.Gender,
+					PlanetId = ch.PlanetId,
+					Planet = ch.Planet
+				})
+				.ToListAsync();
 
-            var characterAllQueryModels = new CharacterQueryServiceModel()
-            {
-                TotalCharactersCount = totalCharactersCount,
-                Characters = charactersViewModels
-            };
+			var characterAllQueryModels = new CharacterQueryServiceModel()
+			{
+				TotalCharactersCount = totalCharactersCount,
+				Characters = charactersViewModels
+			};
 
-            return characterAllQueryModels;
-        }
-    }
+			return characterAllQueryModels;
+		}
+	}
 }
